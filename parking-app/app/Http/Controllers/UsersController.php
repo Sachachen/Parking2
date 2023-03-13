66cf738a597b16fde1da864939c45888eaf5f0dc
Heussem <houssam.rabehi@hotmail.com>
2023-03-13 16:59:09 +0100

    view user (admin)

--- parking-app/app/Http/Controllers/UsersController.php
+++ parking-app/app/Http/Controllers/UsersController.php
@@ -2,12 +2,17 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UsersController extends Controller
 {
     public function index()
     {
-        return view('pages/users');
+        $users = User::all();
+
+        return view('pages/users', [
+            'users' => $users,
+        ]);
     }
 }
